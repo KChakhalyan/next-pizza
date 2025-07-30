5e0cb0aac96482b25da605a728214fb964bec393
KChakhalyan <karenchakhalyan23@gmail.com>
2025-07-30 15:29:10 +0400

    feat: add CheckboxFiltersGroup component and integrate it into Filters

---
+++ components/shared/CheckboxFiltersGroup.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { cn } from '@/lib/utils';
+import React, { useState } from 'react';
+import { FilterChecboxProps, FilterCheckbox } from './FilterCheckbox';
+import { Input } from '../ui';
+
+type Item = FilterChecboxProps;
+
+interface CheckboxFiltersGroupProps {
+    title?: string;
+    items: Item[];
+    defaultItems?: Item[]
+    limit?: number;
+    searchInputPlaceholder?: string;
+    onChange?: (values: string[]) => void;
+    defaultValue?: string[];
+    className?: string;
+
+}
+
+export const CheckboxFiltersGroup: React.FC<CheckboxFiltersGroupProps> = ({
+    title,
+    items,
+    defaultItems,
+    limit = 5,
+    searchInputPlaceholder = 'Search...',
+    onChange,
+    defaultValue,
+    className,
+}) => {
+    const [showAll, setshowAll] = useState(false);
+
+    const itemsList = showAll ? items : items.slice(0, limit);
+
+    return (
+        <div className={cn('', className)}>
+            <p className='font-bold mb-3'>{title}</p>
+
+            {
+                showAll && (
+                    <div className='mb-5'>
+                        <Input placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
+                    </div>
+                )
+            }
+
+            <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'>
+                {itemsList.map((item, index) => (
+                    <FilterCheckbox
+                        key={index}
+                        text={item.text}
+                        value={item.value}
+                        endAdornment={item.endAdornment}
+                        checked={false}
+                        onCheckedChange={(ids) => console.log(ids)}
+                    />
+                ))}
+            </div>
+
+            {items.length > limit && (
+                <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+                    <button onClick={() => setshowAll(!showAll)} className="text-primary mt-3 cursor-pointer">
+                        {showAll ? 'Show Less' : '+ Show All'}
+                    </button>
+                </div>
+            )}
+
+        </div>
+    );
+};
--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -4,6 +4,8 @@ import { Title } from './Title';
 import { FilterCheckbox } from './FilterCheckbox';
 import { Input } from '../ui';
 import { RangeSlider } from './RangeSlider';
+import { Check } from 'lucide-react';
+import { CheckboxFiltersGroup } from './CheckboxFiltersGroup';
 
 interface FiltersProps {
     className?: string;
@@ -13,10 +15,12 @@ export const Filters: React.FC<FiltersProps> = ({ className }) => {
     return (
         <div className={cn('', className)}>
             <Title text="Filters" size="sm" className="mb-5 font-bold" />
+            {/* Upper Checkboxes */}
             <div className='flex flex-col gap-4'>
                 <FilterCheckbox text="Can Takeaway" value="1" />
                 <FilterCheckbox text="New" value="2" />
             </div>
+            {/* Filters with RangeSlidr */}
             <div className='mt-5 border-y border-neutral-100 py-6 pb-7'>
                 <p className='font-bold mb-3'>Price</p>
                 <div className='flex gap-3 mb-5'>
@@ -25,6 +29,51 @@ export const Filters: React.FC<FiltersProps> = ({ className }) => {
                 </div>
                 <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
             </div>
+            {/* Checkbox Filters Group */}
+
+            <CheckboxFiltersGroup
+                title='Ingredients'
+                className='mt-5'
+                limit={6}
+                defaultItems={[
+                    { text: 'Cheese', value: 'cheese' },
+                    { text: 'Pepperoni', value: 'pepperoni' },
+                    { text: 'Mushrooms', value: 'mushrooms' },
+                    { text: 'Onions', value: 'onions' },
+                    { text: 'Sausage', value: 'sausage' },
+                    { text: 'Bacon', value: 'bacon' },
+                    { text: 'Extra cheese', value: 'extra_cheese' },
+                ]}
+                items={[
+                    { text: 'Olives', value: 'olives' },
+                    { text: 'Green peppers', value: 'green_peppers' },
+                    { text: 'Pineapple', value: 'pineapple' },
+                    { text: 'Spinach', value: 'spinach' },
+                    { text: 'Tomatoes', value: 'tomatoes' },
+                    { text: 'Garlic', value: 'garlic' },
+                    { text: 'Jalapeños', value: 'jalapeños' },
+                    { text: 'Olives', value: 'olives' },
+                    { text: 'Green peppers', value: 'green_peppers' },
+                    { text: 'Pineapple', value: 'pineapple' },
+                    { text: 'Spinach', value: 'spinach' },
+                    { text: 'Tomatoes', value: 'tomatoes' },
+                    { text: 'Garlic', value: 'garlic' },
+                    { text: 'Jalapeños', value: 'jalapeños' },
+                    { text: 'Olives', value: 'olives' },
+                    { text: 'Green peppers', value: 'green_peppers' },
+                    { text: 'Pineapple', value: 'pineapple' },
+                    { text: 'Spinach', value: 'spinach' },
+                    { text: 'Tomatoes', value: 'tomatoes' },
+                    { text: 'Garlic', value: 'garlic' },
+                    { text: 'Jalapeños', value: 'jalapeños' },
+                    { text: 'Olives', value: 'olives' },
+                    { text: 'Green peppers', value: 'green_peppers' },
+                    { text: 'Pineapple', value: 'pineapple' },
+                    { text: 'Spinach', value: 'spinach' },
+                    { text: 'Tomatoes', value: 'tomatoes' },
+                    { text: 'Garlic', value: 'garlic' },
+                    { text: 'Jalapeños', value: 'jalapeños' },
+                ]} />
         </div>
     );
 };
