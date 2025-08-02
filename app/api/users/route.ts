8b85e844948b822ef8b316a34a58d826a1938c4b
KChakhalyan <karenchakhalyan23@gmail.com>
2025-08-02 21:33:18 +0400

    refactor: clean up User model and update relationships; add new fields to Order and VerificationCode models

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: Request) {
     const users = await prisma.user.findMany()
     return NextResponse.json({ users })
 }
-
 export async function POST(req: NextRequest) {
     const data = await req.json();
     const user = await prisma.user.create({
